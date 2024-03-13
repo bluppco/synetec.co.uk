bea28543e7a2225f1280c2ce04b00dca6d10a344
Tanish Sharma <tanishsharmaab17@gmail.com>
2024-03-13 16:49:08 +0530

    fix: fixed contact component

--- src/components/home/contact/contact-us/form/index.jsx
+++ src/components/home/contact/contact-us/form/index.jsx
@@ -106,7 +106,7 @@ const ContactForm = ( props ) => {
     }
 
     return (
-        <section className="pt-10 pb-6">
+        <section className="py-6">
             <div className="space-y-2">
                 <div className="space-y-6">
                     <div className="grid grid-cols-1 md:grid-cols-2 gap-x-4 gap-y-6">
