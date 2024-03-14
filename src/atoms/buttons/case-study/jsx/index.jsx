b7b39fb5543c852b8a21c897816e27031ee58b92
Tanish Sharma <tanishsharmaab17@gmail.com>
2024-03-14 16:03:34 +0530

    fix; button width for large screens

--- src/atoms/buttons/case-study/jsx/index.jsx
+++ src/atoms/buttons/case-study/jsx/index.jsx
@@ -1,7 +1,7 @@
 const CaseStudyButton = ( props ) => {
 
     return(
-        <button className="w-full text-center h-16 flex items-center justify-center px-8 rounded text-synetec_white bg-transparent border-2 border-synetec_orange hover:bg-synetec_orange transition-all duration-200 text-lg font-semibold">
+        <button className="w-full md:w-fit text-center h-16 flex items-center justify-center px-8 rounded text-synetec_white bg-transparent border-2 border-synetec_orange hover:bg-synetec_orange transition-all duration-200 text-lg font-semibold">
             { props.children }
         </button>
 
